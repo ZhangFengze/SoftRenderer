53e2ab51e4f710194bb5fbe90bc49cd6e28080f6
crazyZhang <917724035@qq.com>
2021-10-17 00:19:44 +0800

    Triangle1

--- Rasterizer.h
+++ Rasterizer.h
@@ -68,4 +68,67 @@ namespace render
             }
         }
     }
+
+    struct _Point
+    {
+        Vector2 pos;
+        Vector3 barycentric;
+    };
+
+    template<typename Func>
+    void _Triangle1(_Point a, _Point b, _Point c, Func func)
+    {
+        float a_bc = (a.pos - b.pos).Cross(b.pos - c.pos);
+        float b_ac = (b.pos - a.pos).Cross(a.pos - c.pos);
+        if (AlmostZero(a_bc) || AlmostZero(b_ac))
+            return;
+
+        float steps = std::abs(a.pos.y - b.pos.y);
+        float y = a.pos.y;
+        float x0 = a.pos.x;
+        float x1 = a.pos.x;
+
+        float yStep = (b.pos.y - a.pos.y) / steps;
+        float x0Step = (b.pos.x - a.pos.x) / steps;
+        float x1Step = (c.pos.x - a.pos.x) / std::abs(c.pos.y - a.pos.y);
+
+        for (float step = 0;step < steps;++step)
+        {
+            for (float x = std::min(x0, x1);x < std::max(x0, x1);++x)
+            {
+                Vector2 p{ std::floor(x) + 0.5f, std::floor(y) + 0.5f };
+                float barycentricA = (p - b.pos).Cross(b.pos - c.pos) / a_bc;
+                if (barycentricA < 0 || barycentricA>1)
+                    continue;
+                float barycentricB = (p - a.pos).Cross(a.pos - c.pos) / b_ac;
+                if (barycentricB < 0 || barycentricB>1)
+                    continue;
+                float barycentricC = 1 - barycentricA - barycentricB;
+                if (barycentricC < 0 || barycentricC>1)
+                    continue;
+                auto ba = barycentricA * a.barycentric + barycentricB * b.barycentric + barycentricC * c.barycentric;
+				func(p, ba);
+            }
+
+            y += yStep;
+            x0 += x0Step;
+            x1 += x1Step;
+        }
+    }
+
+    template<typename Func>
+    void Triangle1(Vector2 aRaw, Vector2 bRaw, Vector2 cRaw, Func func)
+    {
+        _Point points[3]
+        {
+            {aRaw,{1,0,0}},
+            {bRaw,{0,1,0}},
+            {cRaw,{0,0,1}},
+        };
+        std::sort(std::begin(points), std::end(points),
+            [](const auto& left, const auto& right) {return left.pos.y < right.pos.y;});
+
+        _Triangle1(points[0], points[1], points[2], func);
+        _Triangle1(points[2], points[1], points[0], func);
+    }
 }
