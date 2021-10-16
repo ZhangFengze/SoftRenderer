a96112a3a541b44ded7ff8059293f04126a2678b
crazyZhang <917724035@qq.com>
2021-10-16 10:29:33 +0800

    main loop

--- main.cpp
+++ main.cpp
@@ -4,47 +4,68 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int main(int argc, char* args[])
+bool init();
+void close();
+
+SDL_Window* gWindow = NULL;
+SDL_Surface* gScreenSurface = NULL;
+
+bool init()
 {
-    //The window we'll be rendering to
-    SDL_Window* window = NULL;
+	bool success = true;
 
-    //The surface contained by the window
-    SDL_Surface* screenSurface = NULL;
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+	{
+		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+		success = false;
+	}
+	else
+	{
+		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		if( gWindow == NULL )
+		{
+			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+			success = false;
+		}
+		else
+		{
+			gScreenSurface = SDL_GetWindowSurface( gWindow );
+		}
+	}
 
-    //Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-    }
-    else
-    {
-        //Create window
-        window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        if (window == NULL)
-        {
-            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-        }
-        else
-        {
-            //Get window surface
-            screenSurface = SDL_GetWindowSurface(window);
+	return success;
+}
 
-            //Fill the surface white
-            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+void close()
+{
+	SDL_DestroyWindow( gWindow );
+	gWindow = NULL;
+	SDL_Quit();
+}
 
-            //Update the surface
-            SDL_UpdateWindowSurface(window);
-
-            //Wait two seconds
-            SDL_Delay(2000);
-        }
-    }
-    //Destroy window
-    SDL_DestroyWindow(window);
-
-    //Quit SDL subsystems
-    SDL_Quit();
-
-    return 0;
+int main( int argc, char* args[] )
+{
+	if( !init() )
+	{
+		printf( "Failed to initialize!\n" );
+	}
+	else
+	{
+		bool quit = false;
+		SDL_Event e;
+		while( !quit )
+		{
+			while( SDL_PollEvent( &e ) != 0 )
+			{
+				if( e.type == SDL_QUIT )
+				{
+					quit = true;
+				}
+			}
+            SDL_FillRect(gScreenSurface, NULL, SDL_MapRGB(gScreenSurface->format, 0x00, 0xFF, 0xFF));
+			SDL_UpdateWindowSurface( gWindow );
+		}
+	}
+	close();
+	return 0;
 }
